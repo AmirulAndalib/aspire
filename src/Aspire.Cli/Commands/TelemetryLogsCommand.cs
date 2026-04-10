f64b0ae7ef878ba612403f6191cc08c7c914f44c
James Newton-King <james@newtonking.com>
2026-04-10 10:03:44 +0800

    Fix missing System.Globalization using in TelemetryLogsCommand and TelemetrySpansCommand

--- src/Aspire.Cli/Commands/TelemetryLogsCommand.cs
+++ src/Aspire.Cli/Commands/TelemetryLogsCommand.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.CommandLine;
+using System.Globalization;
 using System.Text.Json;
 using Aspire.Cli.Backchannel;
 using Aspire.Cli.Configuration;
